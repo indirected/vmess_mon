d3986fdbe660793de4252f15349d69cfb48cb245
indirected <46082152+indirected@users.noreply.github.com>
2022-10-28 23:05:07 +0330

    urgent key error bug in update traffic

--- utils.py
+++ utils.py
@@ -333,7 +333,7 @@ def update_traffics(stats: pd.DataFrame):
     upload_updates = []
     download_updates = []
     for user in user_db.index:
-        if stats.loc[stats.index.str.startswith(user)].shape[0] > 0:
+        if stats.loc[f"{user}_uplink"].shape[0] > 0:
             user_up = stats.loc[f"{user}_uplink", 'value']
             user_down = stats.loc[f"{user}_downlink", 'value']
             user_traf = user_up + user_down
